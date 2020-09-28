1a657a2c94d5afb72fe7ee30e359ce74f6043564
Ricardo Augustine <augustineri93@gmail.com>
2020-09-28 02:55:48 -0400

    setup routes

--- server.js
+++ server.js
@@ -10,4 +10,10 @@ const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => res.send("API RUNNING"));
 
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/post', require('./routes/api/post'));
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
