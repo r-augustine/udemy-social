cf996a69bdd90c2b1b3eaf777ac38096924f79fb
Ricardo Augustine <augustineri93@gmail.com>
2020-09-28 03:40:07 -0400

    reagister users now return json web token on success

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../../models/User");
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const User = require("../../models/User");
 const { check, validationResult } = require("express-validator");
 
 // @route   GET api/users
@@ -33,7 +35,9 @@ router.post(
       let user = await User.findOne({ email });
 
       if (user) {
-        return res.status(400).json({ errors: [{ msg: "User already exists" }] });
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
       }
 
       // Get users gravatar
@@ -57,7 +61,20 @@ router.post(
       await user.save();
 
       // Return jsonwebtoken
-      res.send("User registered!");
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(payload, config.get("jwtSecret"), {
+          expiresIn: 3600
+      }, (err, token) => {
+          if(err){
+              throw err;
+          }
+          res.json({ token });
+      });
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server error");
