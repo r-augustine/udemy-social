65d7f510e59d391eefd7fd27809b2164ab59c579
Ricardo Augustine <augustineri93@gmail.com>
2020-09-28 03:12:32 -0400

    added register user route and validation

--- server.js
+++ server.js
@@ -6,14 +6,16 @@ const app = express();
 // Connect Database
 connectDB();
 
-const PORT = process.env.PORT || 5000;
+// Initialize Middleware
+app.use(express.json({ extended: false }));
 
 app.get("/", (req, res) => res.send("API RUNNING"));
 
 // Define Routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/post', require('./routes/api/post'));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/post", require("./routes/api/post"));
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
