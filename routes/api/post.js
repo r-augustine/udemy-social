3d43c515f930ecdfea0698e1a3479b62a65489ed
Ricardo Augustine <augustineri93@gmail.com>
2020-09-29 16:50:19 -0400

    added routes to like and unlike a post

--- routes/api/post.js
+++ routes/api/post.js
@@ -108,6 +108,55 @@ router.delete('/:id', auth, async (req, res) => {
 
         res.status(500).send("Server Error");
     }
+});
+
+// @route   PUT api/post/like/:id
+// @desc    Like a post
+// @access  Private
+router.put('/like/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        // Check if post has already been liked
+        if(post.likes.filter(like => like.user.toString() === req.user.id).length > 0){
+            return res.status(400).json({ errors: [{ msg: 'Post already liked' }]});
+        }
+
+        post.likes.unshift({ user: req.user.id });
+        
+        await post.save();
+
+        res.json(post.likes);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Server Error");
+    }
+})
+
+// @route   PUT api/post/unlike/:id
+// @desc    Unlike a post
+// @access  Private
+router.put('/unlike/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        // Check if post has already been liked
+        if(post.likes.filter(like => like.user.toString() === req.user.id).length == 0){
+            return res.status(400).json({ errors: [{ msg: 'Post has not yet been liked' }]});
+        }
+
+        // Get remove index
+        const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
+
+        post.likes.splice(removeIndex, 1);
+        
+        await post.save();
+
+        res.json(post.likes);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Server Error");
+    }
 })
 
 module.exports = router;
