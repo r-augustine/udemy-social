410096d6991b4d8ab5f8bb71f1713cc9714f6d4e
Ricardo Augustine <augustineri93@gmail.com>
2020-09-29 00:51:07 -0400

    added route to return all profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -100,9 +100,20 @@ router.post(
         console.error(error.message);
         return res.status(400).json('Server Error');
     }
-
-    res.send('Hello');
   }
 );
+
+// @route   GET api/profile
+// @desc    Get all profiles
+// @access  Public
+router.get('/', async (req, res) => {
+    try {
+        const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+        res.json(profiles);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+})
 
 module.exports = router;
