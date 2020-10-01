d1e0ae0013dc62e6784d03bf5874785ff2160b2a
Ricardo Augustine <augustineri93@gmail.com>
2020-09-30 23:01:55 -0400

    redirecting to dashboard if the user is logged in

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,12 +1,12 @@
 import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
 import { register } from "../../actions/auth";
 import PropTypes from 'prop-types';
 
 
-const Register = ({ setAlert, register }) => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
 
     const [formData, setFormData] = useState({
         name: '',
@@ -26,6 +26,11 @@ const Register = ({ setAlert, register }) => {
         } else {
             register({ name, email, password });
         }
+    }
+
+    // Redirect if logged in
+    if(isAuthenticated) {
+        return <Redirect to="/dashboard"/>
     }
 
     return (
@@ -58,4 +63,8 @@ Register.propTypes = {
   register: PropTypes.func.isRequired,
 };
 
-export default connect(null, { setAlert, register })(Register);
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps, { setAlert, register })(Register);
