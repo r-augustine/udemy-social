a86759a4c4652c0bb757b1995c8799eacfc7b967
Ricardo Augustine <augustineri93@gmail.com>
2020-09-30 22:39:07 -0400

    added the user_loaded and auth_error auth actions

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import { Landing } from './components/layout/Landing';
@@ -7,11 +7,23 @@ import Register from './components/auth/Register';
 import { Login } from './components/auth/Login';
 import Alert from './components/layout/Alert';
 
+
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+
+if(localStorage.token) {
+    setAuthToken(localStorage.token);
+}
 
 const App = () => {
+
+    useEffect(() => {
+        store.dispatch(loadUser());
+    },[]);
+
   return (
     <Provider store={store}>
       <Router>
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,4 +1,4 @@
-import { REGISTER_SUCCESS, REGISTER_FAIL } from '../actions/types';
+import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR } from '../actions/types';
 
 const initialState = {
     token: localStorage.getItem('token'),
@@ -12,6 +12,14 @@ export default (state = initialState, action) => {
     const { type, payload } = action;
     
     switch(type){
+        case USER_LOADED:
+            console.log(payload);
+            return {
+                ...state,
+                isAuthenticated: true,
+                loading: false,
+                user: payload
+            }
         case REGISTER_SUCCESS:
             localStorage.setItem('token', payload.token);
             return {
@@ -21,6 +29,7 @@ export default (state = initialState, action) => {
                 loading: false
             }
         case REGISTER_FAIL:
+        case AUTH_ERROR:
             localStorage.removeItem('token');
             return {
                 ...state,
