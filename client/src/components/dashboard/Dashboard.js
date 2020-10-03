74360782bcc5a462052e156388618ae0ab83d3df
Ricardo Augustine <augustineri93@gmail.com>
2020-10-03 12:18:56 -0400

    added Education and Experience components to display users information on the dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -5,6 +5,8 @@ import { connect } from "react-redux";
 import { getCurrentProfile } from "../../actions/profile";
 import Spinner from "../layout/Spinner";
 import DashboardActions from './DashboardActions';
+import Experience from './Experience';
+import Education from './Education';
 
 const Dashboard = ({
   getCurrentProfile,
@@ -23,6 +25,8 @@ const Dashboard = ({
         (
             <Fragment> 
                 <DashboardActions/>
+                <Experience experience={profile.experience}/>
+                <Education education={profile.education}/>
             </Fragment>
         ) : (
             <>
