bd4c341f2b93342c38c95b58153c8197321f960d
Ricardo Augustine <augustineri93@gmail.com>
2020-10-04 07:55:13 -0400

    update Profiles component to show all user profiles

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -3,15 +3,30 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getProfiles } from '../../actions/profile';
+import ProfileItem from './ProfileItem';
 
 const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
     useEffect(() => {
-        getProfiles();
-    },[]);
+      getProfiles();
+    }, [getProfiles]);
     return (
-        <div>
-            
-        </div>
+        <>
+        { loading ? <Spinner/> : 
+            <>
+                <h1 className="large text-primary">Developers</h1>
+                <p className="lead">
+                    <i className="fab fa-connectdevelop"> Browser and connect with developers</i>
+                </p>
+                <div className="profiles">
+                    { profiles.length > 0 ? (
+                        profiles.map(profile => (
+                            <ProfileItem key={profile._id} profile={profile}/>
+                        ))
+                    ) : <h4>No profiles found</h4> }
+                </div>
+            </>
+        }
+        </>
     )
 }
 
