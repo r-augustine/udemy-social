da14cc0e43a0cd1f9ae0e62461f62812b84bf272
Ricardo Augustine <augustineri93@gmail.com>
2020-10-06 20:45:48 -0400

    PostItem view now loading data from redux store

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -3,17 +3,32 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux';
 import { getPosts } from '../../actions/post';
 import Spinner from '../layout/Spinner';
+import PostItem from './PostItem';
 
 const Posts = ({ getPosts, post: { posts, loading }}) => {
     useEffect(() => {
         getPosts();
     }, [getPosts]);
 
-    return (
-        <div>
-            
-        </div>
-    )
+    return loading ? <Spinner/> :  (
+                <>
+                    <h1 className="large">
+                        Posts
+                    </h1>
+                    <p className="lead">
+                        <i className="fas fa-user"></i>
+                        Welcome to the community
+                    </p>
+                    <div className="posts">
+                        {
+                            posts.map(post => (
+                                <PostItem key={post.id} post={post}/>
+                            ))
+                        }
+                    </div>
+                </>
+            )
+ 
 }
 
 Posts.propTypes = {
